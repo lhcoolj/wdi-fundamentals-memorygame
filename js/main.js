177b93a077a88213ebc55132c1aa597e948661ee
Lisa Jones <lisajones@lisas-mac.att.net>
2018-01-06 16:04:43 -0800

    fixed 9 and game logic

--- js/main.js
+++ js/main.js
@@ -1,22 +1,63 @@
 console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "king"];
+
+//in the "cards" array
+//create four objects, one for each card
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
+];
+
+//create an array to store "cardsInPlay"
 var cardsInPlay = [];
 
-var checkForMatch = function (){
-  if (cardsInPlay.length === 2) {
-    if (cardsInPlay[0] === cardsInPlay[1]){
+//create a function to "checkForMatch"
+var checkForMatch = function () {
+  //Check to see if 2 cards match and provide feeback to user
+    if (cardsInPlay[0] === cardsInPlay[1]) {
       console.log("You've found a match.");
     } else {
       console.log("Sorry, try again.");
     }
-  }
-}
+  };
 
+//create a function to represent user flippin a card
 var flipCard = function (cardId) {
+  //display card rank (queen or king) the user just flipped
+  console.log("User flipped " + cards[cardId].rank);
+  //add card to array of cardsInPlay
+  cardsInPlay.push(cards[cardId].rank);
+  //Display card image
+  console.log(cards[cardId].cardImage);
+  //Display card suit
+  console.log(cards[cardId].suit);
+  //check to see if 2 cards have been played
+    if (cardsInPlay.length === 2) {
+      //if so, "checkForMatch"
+      checkForMatch();
+    }
+  };
+
+//For now we are manually calling the flipCard function
+//to represent a user's play
+//Call the flipCard function, passing in an index as the argument
   flipCard(0);
   flipCard(2);
-  console.log("User flipped" + card[cardId]);
-  cardsInPlay.push(card[cardId]);
-  checkForMatch();
-}
